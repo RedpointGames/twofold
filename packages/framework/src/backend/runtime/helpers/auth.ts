6e9e22f00f212dab5d4c2bb5e5f30f5f59769681
June Rhodes <jrhodes@redpoint.games>
2026-05-06 07:47:56 +1000

    Log what auth policies cause access denied

--- packages/framework/src/backend/runtime/helpers/auth.ts
+++ packages/framework/src/backend/runtime/helpers/auth.ts
@@ -12,6 +12,7 @@ import {
 } from "../../auth/auth.js";
 import globalAuth from "virtual:twofold/server-global-auth";
 import { ReplacementResponse } from "../../vite/replacement-response.js";
+import kleur from "kleur";
 
 export async function evaluatePolicyArray(
   node: Node,
@@ -83,6 +84,33 @@ export async function evaluatePolicyArray(
     if (authPolicyResult.__allow) {
       continue;
     } else {
+      let location;
+      switch (props.type) {
+        case "api":
+          location = " [API Route]";
+          break;
+        case "page":
+          location = " [Page]";
+          break;
+        case "action":
+          location = " [Action]";
+          break;
+        case "client-asset":
+          location = " [Client Asset]";
+          break;
+        default:
+          location = "";
+          break;
+      }
+      if (location !== "") {
+        location = kleur["yellow"](location);
+      }
+      console.log(
+        `${kleur["red"](`[Access Denied]`)}%s %s (denied by policy '%s')`,
+        location,
+        props.request.url,
+        authPolicy.name,
+      );
       return authPolicyResult;
     }
   }
